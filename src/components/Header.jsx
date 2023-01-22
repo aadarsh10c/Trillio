82762a1665f102600c5fbfaf4cf86d36a923ec27
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-23 00:47:03 +0530

    started working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import logo from '../assets/logo.png'
 
 export default function Header (){
     return(
         <header className="header">
-            I am Header
+           <img src={logo} alt="" className="icon" />
+           <form action="#" className="search">
+                <input type="text" className="search__box" placeholder='Type to Search'/>
+                <button className='search__btn'>
+                    <svg className="search__icon">
+                        <use href="../src/assets/sprite.svg#icon-magnifying-glass"></use>
+                    </svg>
+                </button>
+           </form>
+           <div className="user-nav">
+            user-nav using svg
+           </div>
         </header>
     );
 }
