6c767549f1a88230e106ef49cbd8b031088a428b
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-22 19:25:39 +0530

    Overall layout style added

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from './components/Header';
-import Content from './components/Content';
+import Header from './Header'
+import Content from './Content';
 
 export default function Container(){
     return (
