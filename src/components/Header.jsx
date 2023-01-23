4e5a70da2e45e4d3b9bfb41f4742a877feeef1f9
Adarsh Kumar <adarsh010c@gmail.com>
2023-01-23 18:17:13 +0530

    Header completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header (){
     return(
         <header className="header">
            <img src={logo} alt="" className="icon" />
+
            <form action="#" className="search">
                 <input type="text" className="search__box" placeholder='Type to Search'/>
                 <button className='search__btn'>
@@ -13,9 +14,25 @@ export default function Header (){
                     </svg>
                 </button>
            </form>
-           <div className="user-nav">
-            user-nav using svg
-           </div>
+
+           <nav className="user-nav">
+            <div className="user-nav__save-box">
+                <svg className="user-nav__save-icon">
+                    <use href="../src/assets/sprite.svg#icon-bookmark"></use>                  
+                </svg>
+                <span className="user-nav__alert">7</span>
+            </div>
+            <div className="user-nav__chat-box">
+                <svg className="user-nav__chat-icon">
+                    <use href="../src/assets/sprite.svg#icon-chat"></use>                  
+                </svg>
+                <span className="user-nav__alert">13</span>
+            </div>
+            <div className="user-nav__profile">
+                <img src="../src/assets/user-4.jpg" alt="Ron Wesely" className="user-nav__profile-pic" />
+                <span className="user-nav__profile-name">Ron</span>
+            </div>
+           </nav>
         </header>
     );
 }
